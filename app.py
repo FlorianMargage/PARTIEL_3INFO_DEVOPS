c8cf2f42fb5eea8b9c3f107a0e789b684046727b
Florian Margage <florian.margage@hotmail.com>
2025-02-26 11:34:23 +0100

    test: covering app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+"""Faire la somme de 2 nombres."""
 def add(a, b):
-    """Ajoute deux nombres."""
     return a + b
 
+"""Faire la multiplication de 2 nombres."""
 def multiply( x,y ):
     return x*y
 
+"""Faire la division de 2 nombres."""
 def divide(x , y ):
     beta = 0
     if y != 0:
@@ -15,4 +17,4 @@ def greet(name):
     if name =="":
         return "Hello, World!"
     else:
-       return "Hello,"+name
+        return "Hello,"+name
