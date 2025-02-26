7e52113c37a32f2281efd53f692fe590d4522229
Florian Margage <florian.margage@hotmail.com>
2025-02-26 11:37:05 +0100

    test: covering app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-"""Faire la somme de 2 nombres."""
 def add(a, b):
+    """Faire la somme de 2 nombres."""
     return a + b
 
-"""Faire la multiplication de 2 nombres."""
 def multiply( x,y ):
+    """Faire la multiplication de 2 nombres."""
     return x*y
 
-"""Faire la division de 2 nombres."""
 def divide(x , y ):
+    """Faire la division de 2 nombres."""
     beta = 0
     if y != 0:
         return x/y
 
 def greet(name):
-    # GREET FONCTION
+    """Retourne Bonjour suivi du nom passé en paramètre."""
     if name =="":
         return "Hello, World!"
     else:
